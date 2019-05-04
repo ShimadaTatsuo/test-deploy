6a7af079c0a3d4d42007d511a961be900b6dc264
tatsuo <test@co.jp>
2019-05-04 15:54:58 +0900

    fix1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello!'))
 
 app.listen(process.env.PORT || 8080, () => console.log('Example app listening on port xxx!'))
